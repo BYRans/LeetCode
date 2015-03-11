56cdb1b6dab6553021ef8d1440a4fc635b163737
Ransford <emailofdingyu@163.com>
2015-03-11 08:07:05 +0800

    Length Of Longest Substring
    Length Of Longest Substring

---
+++ src/leetCode/LengthOfLongestSubstring.java
@@ -0,0 +1,42 @@
+package leetCode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * @author Ransford TODO Given a string, find the length of the longest
+ *         substring without repeating characters. For example, the longest
+ *         substring without repeating letters for "abcabcbb" is "abc", which
+ *         the length is 3. For "bbbbb" the longest substring is "b", with the
+ *         length of 1.
+ */
+public class LengthOfLongestSubstring {
+	public class Solution {
+		public int lengthOfLongestSubstring(String s) {
+			HashSet<String> charSet = new HashSet<String>();
+			int[] arr = new int[s.length()];
+			for (int i = 0; i < s.length(); i++) {
+				if (!charSet.contains(s.charAt(i) + "")) {
+					charSet.add(s.charAt(i) + "");
+					arr[i] = 1;
+				} else
+					arr[i] = 0;
+			}
+			int max = 0;
+			int count = 0;
+			for (int n : arr) {
+				if (n == 1)
+					count++;
+				else {
+					if (count > max) {
+						max = count;
+						count = 1;
+					}
+				}
+			}
+			if (count > max)
+				max = count;
+			return max;
+		}
+	}
+}
--- src/leetCode/runTest.java
+++ src/leetCode/runTest.java
@@ -1,39 +1,22 @@
 package leetCode;
 
-import leetCode.AddTwoNumbers;
-import leetCode.AddTwoNumbers.Solution;
-import leetCode.AddTwoNumbers.ListNode;
+import leetCode.LengthOfLongestSubstring;
+import leetCode.LengthOfLongestSubstring.Solution;
 
 /**
  * @author Ransford TODO
  */
-public class runTest {
+public class RunTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-//		int[] numbers = { 3, 2, 4 };
-//		int target = 6;
-		
-		AddTwoNumbers ln = new AddTwoNumbers();
-		Solution solution = ln.new Solution();
+		LengthOfLongestSubstring test = new LengthOfLongestSubstring();
+		Solution solution = test.new Solution();
+		String s = "dvdf";
+		int score = solution.lengthOfLongestSubstring(s);
 
-		
-		// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4) Output: 7 -> 0 -> 8
-		ListNode root1 = ln.new ListNode(3);
-		root1.next = ln.new ListNode(7);
-//		root1.next.next = ln.new ListNode(1);
-
-		ListNode root2 = ln.new ListNode(9);
-		root2.next = ln.new ListNode(2);
-//		root2.next.next = ln.new ListNode(4);
-
-		ListNode score = solution.addTwoNumbers(root1, root2);
-
-		while (score != null){
-			System.out.print(score.val + "-->");
-			score = score.next;
-		}
+		System.out.print(score);
 	}
 
 }
