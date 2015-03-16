1306302c7377591947461ce567520ae698620e1a
Ransford <dingyugege@sina.com>
2015-03-17 00:41:34 +0800

    Longest Substring Without Repeating Characters
    Longest Substring Without Repeating Characters completed.

--- src/leetCode/LengthOfLongestSubstring.java
+++ src/leetCode/LengthOfLongestSubstring.java
@@ -22,18 +22,21 @@ public class LengthOfLongestSubstring {
 			int max = 0;
 			int begin = 0;
 			for (int i = 0; i < cArr.length; i++) {
-				if (map.get(cArr[i] + "") == null) {
-					map.put(cArr[i] + "", 0);
-				} else
-					map.put(cArr[i] + "", map.get(cArr[i] + "") + 1);
-
-				while (map.get(cArr[i] + "") >= 1) {
-					map.put(cArr[i] + "", map.get(cArr[i] + "") - 1);
-					begin++;
+				String c = cArr[i] + "";
+				if (!map.containsKey(c)) {
+					map.put(c, i);
+				} else {
+					int index = map.get(c);
+					map.put(c, i);
+					if (i - begin > max)
+						max = i - begin;
+					if (index + 1 > begin)
+						begin = index + 1;
 				}
-				if (i - begin + 1 > max)
-					max = i - begin + 1;
 			}
+			if (cArr.length - begin > max)
+				max = cArr.length - begin;
+
 			return max;
 		}
 	}
