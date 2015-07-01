b19534fb80522633d08bf5b2f9550c3b72d3e813
Ransford <dingyugege@sina.com>
2015-07-01 16:39:55 +0800

    add Reverse Integer Version 2
    add Reverse Integer Version 2
    Version 2 is more elegant than Version 1

--- Python/ReverseInteger.py
+++ Python/ReverseInteger.py
@@ -5,22 +5,20 @@ class Solution:
 	# @param {integer} x
 	# @return {integer}
 	def reverse(self, x):
-		while x%10 == 0 and x != 0:
+		flag = 1
+		result = 0
+		if x < 0:
+			flag = -1
+			x = -x
+		while x != 0:
+			result = result*10 + x%10
 			x /= 10
-		if x >= 0:
-			xStrReverse = str(x)[::-1]
-			xReverse = int(xStrReverse)
-			if xReverse > 2147483647:
-				return 0
-			return xReverse
-		else:
-			xStrReverse = str(x)[:0:-1]
-			xReverse = -int(xStrReverse)
-			if xReverse < -2147483648:
-				return 0
-			return xReverse
+		if result > 2147483647 or result < -2147483648: 
+			return 0
+		return flag * result
+
 
 
 solution = Solution()
-result = solution.reverse(-10)
+result = solution.reverse(-12)
 print result
